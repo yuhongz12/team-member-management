1f51fc7851b438c82fdb3864a038c07ae8f6bec5
yuhongz12 <yuhong.zhao12@gmail.com>
2022-11-14 16:11:56 -0500

    add create member button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 import MemberList from "./member-list";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {createMember} from "./reducers/members-reducer";
 
 
 
 function App() {
 
-  const memberArray = useSelector(state => state.members)
-
+  const memberArray = useSelector(state => state.members);
+  const dispatch = useDispatch();
+  const addMemberHandler = () => {
+    const newMember = {
+      firstName: "bob",
+      lastName: "smith",
+      email: "bob@gmail.com",
+      phoneNumber: "131-123-1233",
+      role: "regular"
+    }
+    dispatch(createMember(newMember));
+    console.log()
+  }
   return (
 
       <div className='container'>
         <h1>Team members</h1>
+        <button onClick={addMemberHandler}>Add</button>
         <p className={'text-muted'}>You have {memberArray.length} team members.</p>
         <MemberList/>
       </div>
--- src/member-list/index.js
+++ src/member-list/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import members from "../data/members";
 import MemberItem from "./member-item";
+import {useSelector} from "react-redux";
 
 const MemberList = () => {
+    const members = useSelector(state => state.members);
     return (
         <ul className={'list-group'}>
             {members.map(member => <MemberItem member={member}/>)}
